40233e4e5d0e290bc62847e3c829b36f53e3259d
mahe1999mh <mahe1999mh@gmail.com>
2024-05-27 21:05:05 +0530

    changes 2

--- routes/auth/singup.js
+++ routes/auth/singup.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const User = require("../../models/userModel");
 const router = express.Router();
+const express = require("express");
 
 router.post("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const newUser = new User({ email, password, firstName, lastName });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
