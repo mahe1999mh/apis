af5e6b0c512ecd36b5121757841ecdae886c19be
mahe1999mh <mahe1999mh@gmail.com>
2024-05-27 21:09:43 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const home = require("./routes/home");
 const sigin = require("./routes/auth/singin")
-// const sigup = require("./routes/auth/singup")
+const sigup = require("./routes/auth/singup")
 
 // Middlewares
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 // Routes
 app.use("/api/home", home);
 app.use("/api/singin",sigin)
-// app.use("/api/singup",sigup)
+app.use("/api/singup",sigup)
 
 // connection
 const port = process.env.PORT || 9001;
