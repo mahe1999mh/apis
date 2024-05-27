2d77b37a6c1ff2238cf37b5a7ff04fb43a102591
mahe1999mh <mahe1999mh@gmail.com>
2024-05-27 20:50:02 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use("/api/home", home);
+app.use("/api/singin")
+app.use("/api/singup")
 
 // connection
 const port = process.env.PORT || 9001;
